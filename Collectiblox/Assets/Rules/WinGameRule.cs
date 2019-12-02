f75d41bfbed30cb0bf5862ff1e3752886157257f
Paul Redetzky <p_redetzky@gmx.de>
2019-12-02 21:09:10 +0100

    moved rules

---
+++ Collectiblox/Assets/Rules/WinGameRule.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Collectiblox.Model.Commands;
+using UnityEngine;
+
+namespace Collectiblox.Model.Rules
+{
+    public class WinGameRule : Rule
+    {
+        public override void Init()
+        {
+        }
+
+        public override RuleEvaluationInfo IsCommandExecutable(GameManager gm, ICommand command)
+        {
+            return new RuleEvaluationInfo();
+        }
+
+        public override RuleEvaluationInfo IsCommandSendable(GameManager gm, ICommand command)
+        {
+            return new RuleEvaluationInfo();
+
+        }
+
+        bool IsGameOver(GameManager gm, ICommand command)
+        {
+            return false;
+        }
+    }
+}
